d9155364466659be0e0cea1eba4212e993841a8d
devalmanu <manuilova-1986@mail.ru>
2023-08-03 10:45:22 +0300

    finish page work

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,3 +3,4 @@ import './components/sliders.js';
 import './components/progress.js';
 import './components/to-top.js';
 import './components/accordions.js';
+import './components/portfolio-tabs.js';

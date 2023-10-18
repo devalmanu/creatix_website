14023e5035dea2055660aa9938f5d93cdc9c5185
devalmanu <manuilova-1986@mail.ru>
2023-10-18 14:33:19 +0300

    fix modal position

--- src/js/components/selects.js
+++ src/js/components/selects.js
@@ -1,6 +1,6 @@
 const selects = document.querySelectorAll('.form-field__select');
 selects.forEach(el => {
-  console.log(el)
+  // console.log(el)
   new Choices(el, {
     shouldSort: false,
     position: 'bottom',
